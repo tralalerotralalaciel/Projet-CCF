d7823fe346ff01127f81e9fbccc8ca8e725541ee
Aïdan Bouguernine-Dornet <aidan@lavoisier.local>
2025-12-16 10:16:34 +0100

    Création des fichiers test

---
+++ test_connection/test_connection.ino
@@ -0,0 +1,26 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char ssid = "WIFI_FOR_ESP32"
+const char password = "WIFI_FOR_ESP32"
+
+// const char serverName = ""
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+
+  // Pendant la connexion WiFi
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("\nConnected to WiFi network with IP Address : " + WiFi.localIP());
+}
+
+void loop() {
+
+}
