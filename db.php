b67f40512f0bff8ebabb0cfda36171375d928653
Rezox2Code <osmo.eyal24@gmail.com>
2025-12-18 14:17:19 +0100

    final commit

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db   = 'projet_salle';
-$user = 'root';
-$pass = '';
+$user = 'phpmyadmin';
+$pass = '****';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
